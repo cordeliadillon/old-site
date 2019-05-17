97873a166d92944aee7db67c4ba53f39b37e2719
Cordelia <cordeliadillon@users.noreply.github.com>
2019-05-17 07:26:13 -0700

    Update data.js

--- talks/data.js
+++ talks/data.js
@@ -82,7 +82,7 @@ talks.controller('TalksController', function($scope) {
   $scope.upcomingTalks = [
      {
        id: '051420',
-       title: '(I\'m stoked to be cohosting this conf!)',
+       title: 'Co-hosting with Eric Bailey',
        eventDate: 'October 24-25, 2019',
        eventName: '#a11yTOConf',
        eventHref: 'https://conf.a11yto.com/',
